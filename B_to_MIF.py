c6c35154adba604cc0490c42d95ecf3fa7aa1bc8
AllanCh <edochacon96@gmail.com>
2020-06-19 23:06:28 -0600

    Corrección Generadores .mif

--- B_to_MIF.py
+++ B_to_MIF.py
@@ -1,18 +1,18 @@
 fuente = open("CodigoBinario.bin","r")
 mif = open("CodigoMif.mif","w")
 
-mif.write("DEPTH = 32;\n")
-mif.write("WIDTH = 8; \n")
-mif.write("ADDRESS_RADIX = HEX; \n")
+mif.write("DEPTH = 75;\n")
+mif.write("WIDTH = 32; \n")
+mif.write("ADDRESS_RADIX = DEC; \n")
 mif.write("DATA_RADIX = BIN;  \n")
 mif.write("CONTENT\n")
 mif.write("BEGIN\n")
 
 contdir = 0
 while True:
-    palabra = fuente.read(8)
+    palabra = fuente.read(32)
     if not palabra: break
-    resultado = str(contdir) + " : " + palabra
+    resultado = str(contdir) + " : " + palabra+";"
     mif.write(resultado)
     mif.write("\n")
     contdir+=1
--- P_to_MIF.py
+++ P_to_MIF.py
@@ -1,9 +1,9 @@
 fuente = open("3.txt","r")
 mif = open("PixelMif.mif","w")
 
-mif.write("DEPTH = 32;\n")
-mif.write("WIDTH = 8; \n")
-mif.write("ADDRESS_RADIX = HEX; \n")
+mif.write("DEPTH = 614408;\n")
+mif.write("WIDTH = 32; \n")
+mif.write("ADDRESS_RADIX = DEC; \n")
 mif.write("DATA_RADIX = BIN;  \n")
 mif.write("CONTENT\n")
 mif.write("BEGIN\n")
@@ -11,7 +11,7 @@ mif.write("BEGIN\n")
 contdir = 0
 for pixel in fuente:
     for valor in pixel.split():
-        resultado = str(contdir) + " : " + str(bin(int(valor))[2:].zfill(8))
+        resultado = str(contdir) + " : " + str(bin(int(valor))[2:].zfill(32))+";"
         mif.write(resultado)
         mif.write("\n")
         contdir+=1
