6335b6b74b6134d8421e4362ebeb5888fe6e63c3
Gabriela Ávila <magaby.tec@gmail.com>
2020-06-21 14:47:25 -0600

    Cambio de los nombres

--- RegEXMEM_tb.sv
+++ RegEXMEM_tb.sv
@@ -2,15 +2,15 @@ module RegEXMEM_tb();
 
 logic clk = 1;
 logic [4:0] OpCode, OpCodeOut;
-logic [8:0] Rd, RdOut;
+logic [8:0] RdOut, RdOutOut;
 logic [6:0]  BranchResult, BranchResultOut;
-logic [31:0] ResultAlu, ResultAluOut;
+logic [31:0] AluResult, AluResultOut;
 
 
 
-RegEXMEM RegEXMEM(.clk(clk), .OpCode(OpCode), .Rd(Rd), .BranchResult(BranchResult), 
-.ResultAlu(ResultAlu), .OpCodeOut(OpCodeOut), .RdOut(RdOut), .BranchResultOut(BranchResultOut), 
-.ResultAluOut(ResultAluOut));
+RegEXMEM RegEXMEM(.clk(clk), .OpCode(OpCode), .RdOut(RdOut), .BranchResult(BranchResult), 
+.AluResult(AluResult), .OpCodeOut(OpCodeOut), .RdOutOut(RdOutOut), .BranchResultOut(BranchResultOut), 
+.AluResultOut(AluResultOut));
 
 
 always 
@@ -19,22 +19,22 @@ always
 
 initial begin
 OpCode = 5'd3;
-Rd = 9'd8;
+RdOut = 9'd8;
 BranchResult = 7'd1;
-ResultAlu = 32'd2;
+AluResult = 32'd2;
 
 #10
 OpCode = 5'd11;
-Rd = 9'd7;
+RdOut = 9'd7;
 BranchResult = 7'd6;
-ResultAlu = 32'd9;
+AluResult = 32'd9;
 
 
 #10
 OpCode = 5'd7;
-Rd = 9'd11;
+RdOut = 9'd11;
 BranchResult = 7'd9;
-ResultAlu = 32'd7;
+AluResult = 32'd7;
 
 end
 
