c82203a176d2afb1aa135c729ecba3e0e52ed14e
Gabriela Ávila <magaby.tec@gmail.com>
2020-06-21 14:47:42 -0600

    Se cambio el nombre

--- RegEXMEM.sv
+++ RegEXMEM.sv
@@ -1,34 +1,34 @@
 module RegEXMEM(
 input  logic clk,
 input  logic [4:0] OpCode,
-input  logic [8:0] Rd,
+input  logic [8:0] RdOut,
 input  logic [6:0]  BranchResult,
-input  logic [31:0]  ResultAlu,
+input  logic [31:0]  AluResult,
 output logic [4:0] OpCodeOut,
-output  logic [8:0] RdOut,
+output  logic [8:0] RdOutOut,
 output logic [6:0]  BranchResultOut,
-output logic [31:0]  ResultAluOut
+output logic [31:0]  AluResultOut
 );
 
 
 logic [4:0] VOpCode;
-logic [8:0] VRd;
+logic [8:0] VRdOut;
 logic [6:0]  VBranchResult;
-logic [31:0]  VResultAlu;
+logic [31:0]  VAluResult;
 
 	always @(posedge clk)
 	begin
 			OpCodeOut <= VOpCode;
-			RdOut <= VRd;
+			RdOutOut <= VRdOut;
 			BranchResultOut <= VBranchResult;
-			ResultAluOut <= VResultAlu;
+			AluResultOut <= VAluResult;
 			
 	end 
 	always @(negedge clk)
 	begin
 		   VOpCode <= OpCode;
-			VRd <= Rd;
-			VResultAlu <= ResultAlu;
+			VRdOut <= RdOut;
+			VAluResult <= AluResult;
 			VBranchResult <= BranchResult;
 	end
 
