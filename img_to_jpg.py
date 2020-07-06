8b26be0329e829244112ef05aca4b50db4ca280a
Gabriela Ávila <magaby.tec@gmail.com>
2020-07-05 18:49:29 -0600

    pasar de .img a imagen
    simulador del vga

---
+++ img_to_jpg.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+encriptado = open('Encriptado.img','r')
+desencriptado = open('Desencriptado.img','r')
+encriptadoMatriz = [[]]
+desencriptadoMatriz = [[]]
+i = 0
+contadorAncho = 0
+for linea in encriptado:
+    contadorColumna = 1
+    if(linea[-1] != "x" and contadorAncho == 480):
+        encriptadoMatriz.append([])
+        i += 1
+        contadorAncho = 0
+    for valor in linea.split():
+        if(contadorColumna == 6 and valor != "xxxxxxxx"):
+            encriptadoMatriz[i].append(int(valor,2))
+            contadorAncho += 1
+        contadorColumna += 1
+print(np.asarray(encriptadoMatriz).shape)
+
+i = 0
+contadorAncho = 0
+for linea in desencriptado:
+    contadorColumna = 1
+    if(linea[-1] != "x" and contadorAncho == 480):
+        desencriptadoMatriz.append([])
+        i += 1
+        contadorAncho = 0
+    for valor in linea.split():
+        if(contadorColumna == 6 and valor != "xxxxxxxx"):
+            desencriptadoMatriz[i].append(int(valor,2))
+            contadorAncho += 1
+        contadorColumna += 1
+
+
